92e13e7645f8109f7c8b159b982df9b665705803
Florencia Araí Frettes <167808048+AraaaAraa@users.noreply.github.com>
2025-05-26 18:37:07 -0300

    Create 1.py

---
+++ Primer cuatrimestre/Funciones/1.py
@@ -0,0 +1,9 @@
+""" 1-Crear una función que le solicite al usuario el ingreso de un número entero y lo retorne. """
+
+def numero (ingreso):
+    print(ingreso)
+
+
+ingreso = int(input("Ingrese un numero: "))
+
+numero(ingreso)
