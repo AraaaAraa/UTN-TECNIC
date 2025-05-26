fa7c8e34a65ffe26698660254685459960d77d9d
Florencia Araí Frettes <167808048+AraaaAraa@users.noreply.github.com>
2025-05-26 18:45:48 -0300

    Create 3.py

---
+++ Primer cuatrimestre/Funciones/3.py
@@ -0,0 +1,8 @@
+""" 3-Crear una función que le solicite al usuario el ingreso de una cadena y la retorne.  """
+
+def texto (ingreso):
+    print(ingreso)
+
+ingreso = str(input("Ingrese su texto: "))
+
+texto(ingreso)
