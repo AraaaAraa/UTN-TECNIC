743b930a108311042dc148e8f2c399d4c8cf0104
Florencia Araí Frettes <167808048+AraaaAraa@users.noreply.github.com>
2025-05-26 18:40:30 -0300

    Create 2.py

---
+++ Primer cuatrimestre/Funciones/2.py
@@ -0,0 +1,8 @@
+""" 2-Crear una función que le solicite al usuario el ingreso de un número flotante y lo retorne. """
+
+def numero (ingreso):
+    print(ingreso)
+
+ingreso = float(input("Ingrese un numero: "))
+
+numero(ingreso)
