9150257f6f156909095418a05ef88ddef83c2e0c
Florencia Araí Frettes <167808048+AraaaAraa@users.noreply.github.com>
2025-05-26 19:18:35 -0300

    Create 5.py

---
+++ Primer cuatrimestre/Funciones/5.py
@@ -0,0 +1,9 @@
+""" 5-Escribe una función que calcule el área de un círculo. La función debe recibir el radio como parámetro y devolver el área. """
+
+def circulo(radio):
+    calculo = 3.14*(radio * radio)
+    print(calculo)
+
+radio = int(input("Ingrese el radio del circulo: "))
+
+circulo (radio)
